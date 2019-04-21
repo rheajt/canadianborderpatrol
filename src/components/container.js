3432acf9e3e78016c1b52d3743f07790dc16742c
Ryan Oldford <ryan.oldford@gmail.com>
2019-04-21 11:25:54 +0800

    Updated Container component to fix window size and overall margins

--- src/components/container.js
+++ src/components/container.js
@@ -7,8 +7,10 @@ class Container extends React.Component {
       <Box
         {...this.props}
         mx='auto'
+        p={2}
         css={{
-          maxWidth: '1024px'
+          maxWidth: '1024px', 
+          minWidth: '320px'
         }}
       />
     )
