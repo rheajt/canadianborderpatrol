d07242b7f726c534143edadfccb523f404353c4e
Ryan Oldford <ryan.oldford@gmail.com>
2019-04-21 14:12:07 +0800

    Divider now has vertical margins

--- src/components/divider.js
+++ src/components/divider.js
@@ -3,7 +3,7 @@ import { Flex, Box } from 'rebass';
 
 const Divider = props => (
   <Flex flexWrap='flex' justifyContent='center'>
-    <Box width={props.width}>
+    <Box width={props.width} my={2}>
       <hr />
     </Box>
   </Flex>
