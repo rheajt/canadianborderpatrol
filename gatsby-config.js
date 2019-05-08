61b36444084d4ea1c72dd1c099b788192777fa11
Ryan Oldford <ryan.oldford@gmail.com>
2019-05-08 20:04:45 +0800

    Adding end comment in Markdown files now ends the excerpt, so we don't have all the notes links coming through

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,6 +42,7 @@ module.exports = {
     {
       resolve: `gatsby-transformer-remark`,
       options: {
+        "excerpt_separator": `<!-- end -->`, 
         plugins: [
           {
             resolve: `gatsby-remark-images`,
